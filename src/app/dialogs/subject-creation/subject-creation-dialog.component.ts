b34c647200b076ada66a0b7f19c3b322520a8072
trugaaa <andrey.trugaaa@gmail.com>
2021-11-02 21:26:21 +0200

    Validation on subscription dialog creation

--- src/app/dialogs/subject-creation/subject-creation-dialog.component.ts
+++ src/app/dialogs/subject-creation/subject-creation-dialog.component.ts
@@ -1,4 +1,7 @@
 import {Component} from "@angular/core";
+import {AuthApiService} from "../../services/auth-api.service";
+import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: "subject-creation-dialog",
@@ -6,5 +9,16 @@ import {Component} from "@angular/core";
   styleUrls: ["subject-creation-dialog.component.scss"]
 })
 export class SubjectCreationDialogComponent {
+  constructor(private apiService: AuthApiService, private router: Router) {
+  }
 
+  subjectCreationForm = new FormGroup({
+    name: new FormControl("", Validators.required),
+    course: new FormControl("", Validators.required),
+    description: new FormControl("", Validators.required)
+  });
+
+  onCreateSubject() {
+    this.router.navigate([`subject/new/lesson/new`]);
+  }
 }
--- src/app/dialogs/subject-creation/subject-creation-dialog.module.ts
+++ src/app/dialogs/subject-creation/subject-creation-dialog.module.ts
@@ -1,6 +1,9 @@
 import {NgModule} from "@angular/core";
 import {MatSharedModule} from "../../shared/mat.shared.module";
 import {SubjectCreationDialogComponent} from "./subject-creation-dialog.component";
+import {RouterModule} from "@angular/router";
+import {MainPageComponent} from "../../pages/main/main-page.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [SubjectCreationDialogComponent],
@@ -8,7 +11,9 @@ import {SubjectCreationDialogComponent} from "./subject-creation-dialog.componen
     SubjectCreationDialogComponent
   ],
   imports: [
-    MatSharedModule
+    MatSharedModule,
+    RouterModule.forChild([{path: '**', component: MainPageComponent}]),
+    ReactiveFormsModule
   ]
 })
 export class SubjectCreationDialogModule {
